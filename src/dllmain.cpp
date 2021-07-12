2e6975775341cda0cbe308ae4d7ed27870097ad7
checkraisefold <19525688+checkraisefold@users.noreply.github.com>
2021-07-11 20:42:32 -0700

    Project cleanup w/ ReSharper

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -5,45 +5,49 @@
 #include <mumble/MumblePlugin_v_1_0_x.h>
 #include <json/json.hpp>
 
+#include <functional>
+#include <thread>
 #include <set>
-
 #ifdef OS_UNIX
 	#include <unistd.h>
+#else
+	#include <windows.h>
 #endif
 
 using Server = websocketpp::server<websocketpp::config::asio>;
 using MessagePtr = Server::message_ptr;
-using json = nlohmann::json;
+using Json = nlohmann::json;
 
 using websocketpp::lib::placeholders::_1;
 using websocketpp::lib::placeholders::_2;
 using websocketpp::lib::bind;
 
-std::thread serverThread;
-
-union positions
+union Positions
 {
-	float values[18] = { };
-	struct {
+	float values[18] = {};
+
+	struct
+	{
 		float avPos[3], avFront[3], avTop[3], cmPos[3], cmFront[3], cmTop[3];
 	};
-} pos;
+} positions;
 
-uint64_t gameID;
+uint64_t gameId;
 std::string userContext;
 std::string userIdent;
 
-struct MumbleAPI_v_1_0_x mumbleAPI;
-mumble_plugin_id_t ownID;
+struct MumbleAPI_v_1_0_x mumbleApi;
+mumble_plugin_id_t ownId;
 
 Server posServer;
 std::set<websocketpp::connection_hdl, std::owner_less<websocketpp::connection_hdl>> connections;
 bool initializedServerBefore = false;
+std::thread serverThread;
 
 #ifdef OS_UNIX
 	uint64_t robloxPid;
 #else
-	HANDLE robloxProcHandle;
+HANDLE robloxProcHandle;
 #endif
 
 // Define a callback to handle incoming messages
@@ -52,28 +56,33 @@ void onMessage(Server* s, websocketpp::connection_hdl hdl, MessagePtr msg)
 	// Store connection
 	connections.emplace(hdl);
 
-	// Parse received json, write values to positions array
-	json decoded = json::parse(msg->get_payload());
+	// Parse received JSON, write values to positions array
+	Json decoded = Json::parse(msg->get_payload());
 	for (int i = 0; i < 18; ++i)
-		pos.values[i] = decoded[i / 3][i % 3];
+		positions.values[i] = decoded[i / 3][i % 3];
 
 	// ROBLOX Studs -> Meters conversion
-	for (int i = 0; i < 3; ++i) {
-		pos.avPos[i] *= 0.28f;
-		pos.cmPos[i] *= 0.28f;
+	for (int i = 0; i < 3; ++i)
+	{
+		positions.avPos[i] *= 0.28f;
+		positions.cmPos[i] *= 0.28f;
 	}
 
-	gameID = decoded[6];
+	gameId = decoded[6];
 	userIdent = decoded[7];
 }
 
-void threadLoop() {
-	try {
+void threadLoop()
+{
+	try
+	{
 		// If we've started this thread before, prepare the asio io_service loop to run again
-		if (initializedServerBefore) {
+		if (initializedServerBefore)
+		{
 			posServer.get_io_service().restart();
 		}
-		else {
+		else
+		{
 			initializedServerBefore = true;
 		}
 
@@ -86,25 +95,29 @@ void threadLoop() {
 		// Start the ASIO io_service run loop
 		posServer.run();
 	}
-	catch (websocketpp::exception const& e) {
+	catch (const websocketpp::exception& e)
+	{
 		std::string logMsg = "Websocket thread error (MAKE A GITHUB ISSUE): ";
-		mumbleAPI.log(ownID, (logMsg + e.m_msg).c_str());
+		mumbleApi.log(ownId, (logMsg + e.m_msg).c_str());
 	}
 }
 
-bool gameRunning() {
+bool gameRunning()
+{
 #ifdef OS_UNIX
 	if (getpgid(robloxPid) >= 0) {
 		return false;
 	}
 #else
 	DWORD code = 0;
-	if (GetExitCodeProcess(robloxProcHandle, &code) == 0) {
+	if (GetExitCodeProcess(robloxProcHandle, &code) == 0)
+	{
 		CloseHandle(robloxProcHandle);
 		return false;
 	}
 
-	if (code != STILL_ACTIVE) {
+	if (code != STILL_ACTIVE)
+	{
 		CloseHandle(robloxProcHandle);
 		return false;
 	}
@@ -113,18 +126,20 @@ bool gameRunning() {
 }
 
 // Called by Mumble to fetch positional and rotational character data
-bool mumble_fetchPositionalData(float* avatarPos, float* avatarDir, float* avatarAxis, float* cameraPos, float* cameraDir,
-	float* cameraAxis, const char** context, const char** identity) {	
+bool mumble_fetchPositionalData(float* avatarPos, float* avatarDir, float* avatarAxis, float* cameraPos,
+                                float* cameraDir,
+                                float* cameraAxis, const char** context, const char** identity)
+{
 	// Set data
-	memcpy(avatarPos, pos.avPos, sizeof(pos.avPos));
-	memcpy(avatarDir, pos.avFront, sizeof(pos.avFront));
-	memcpy(avatarAxis, pos.avTop, sizeof(pos.avTop));
-	memcpy(cameraPos, pos.cmPos, sizeof(pos.cmPos));
-	memcpy(cameraDir, pos.cmFront, sizeof(pos.cmFront));
-	memcpy(cameraAxis, pos.cmTop, sizeof(pos.cmTop));
+	memcpy(avatarPos, positions.avPos, sizeof(positions.avPos));
+	memcpy(avatarDir, positions.avFront, sizeof(positions.avFront));
+	memcpy(avatarAxis, positions.avTop, sizeof(positions.avTop));
+	memcpy(cameraPos, positions.cmPos, sizeof(positions.cmPos));
+	memcpy(cameraDir, positions.cmFront, sizeof(positions.cmFront));
+	memcpy(cameraAxis, positions.cmTop, sizeof(positions.cmTop));
 
 	// Set context so it's only for people in the same game
-	userContext = std::to_string(gameID);
+	userContext = std::to_string(gameId);
 	*context = userContext.c_str();
 
 	// Set identity.
@@ -134,26 +149,30 @@ bool mumble_fetchPositionalData(float* avatarPos, float* avatarDir, float* avata
 	return gameRunning();
 }
 
-uint8_t mumble_initPositionalData(const char* const *programNames, const uint64_t *programPIDs, size_t programCount)
+uint8_t mumble_initPositionalData(const char* const * programNames, const uint64_t* programPIDs, size_t programCount)
 {
 	// Check if game is open
-	for (int i = 0; i < programCount; ++i) {
-		if (strcmp(programNames[i], "RobloxPlayerBeta.exe") == 0) {
+	for (int i = 0; i < programCount; ++i)
+	{
+		if (strcmp(programNames[i], "RobloxPlayerBeta.exe") == 0)
+		{
 			// Initialize handle and check whether or not it's valid
-			#ifdef OS_UNIX
+#ifdef OS_UNIX
 				robloxPid = programPIDs[i];
-			#else
-				robloxProcHandle = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, static_cast<DWORD>(programPIDs[i]));
-				if (robloxProcHandle == NULL) {
-					return MUMBLE_PDEC_ERROR_TEMP;
-				}
-#			endif 
+#else
+			robloxProcHandle = OpenProcess(PROCESS_QUERY_INFORMATION, FALSE, static_cast<DWORD>(programPIDs[i]));
+			if (robloxProcHandle == nullptr)
+			{
+				return MUMBLE_PDEC_ERROR_TEMP;
+			}
+#			endif
 
 			// Check if ROBLOX is still open
-			if (!gameRunning()) {
-				#ifndef OS_UNIX
-					CloseHandle(robloxProcHandle);
-				#endif
+			if (!gameRunning())
+			{
+#ifndef OS_UNIX
+				CloseHandle(robloxProcHandle);
+#endif
 				return MUMBLE_PDEC_ERROR_TEMP;
 			}
 
@@ -167,34 +186,40 @@ uint8_t mumble_initPositionalData(const char* const *programNames, const uint64_
 }
 
 // Stop the websocket listening and join the thread
-void mumble_shutdownPositionalData() { 
+void mumble_shutdownPositionalData()
+{
 	// Cleanly stop the websocket server
 	posServer.stop_listening();
 
 	// Close all connections
-	for (auto& connection_hdl : connections) {
-		auto connection = posServer.get_con_from_hdl(connection_hdl);
+	for (auto& connectionHdl : connections)
+	{
+		auto connection = posServer.get_con_from_hdl(connectionHdl);
 
 		// Make sure connection is open before closing it
-		if (connection->get_state() == websocketpp::session::state::open) {
+		if (connection->get_state() == websocketpp::session::state::open)
+		{
 			connection->close(websocketpp::close::status::normal, "Server shutdown");
 		}
 	}
 	connections.clear();
 
 	// Join server thread to main thread
-	if (serverThread.joinable()) {
+	if (serverThread.joinable())
+	{
 		serverThread.join();
 	}
 }
 
 // Plugin initialization routine
-mumble_error_t mumble_init(mumble_plugin_id_t pluginID) {
+mumble_error_t mumble_init(mumble_plugin_id_t pluginID)
+{
 	// Set plugin ID
-	ownID = pluginID;
+	ownId = pluginID;
 
 	// Initialize websocket server
-	try {
+	try
+	{
 		// Set logging settings
 		posServer.set_access_channels(websocketpp::log::alevel::all);
 		posServer.clear_access_channels(websocketpp::log::alevel::frame_payload);
@@ -203,22 +228,26 @@ mumble_error_t mumble_init(mumble_plugin_id_t pluginID) {
 		posServer.init_asio();
 
 		// Register our message handler
-		posServer.set_message_handler(bind(&onMessage, &posServer, ::_1, ::_2));
+		posServer.set_message_handler(bind(&onMessage, &posServer, _1, _2));
 	}
-	catch (websocketpp::exception const& e) {
+	catch (const websocketpp::exception& e)
+	{
 		std::string logMsg = "Plugin init error (MAKE A GITHUB ISSUE): ";
-		mumbleAPI.log(ownID, (logMsg + e.m_msg).c_str());
+		mumbleApi.log(ownId, (logMsg + e.m_msg).c_str());
 	}
 
 	return MUMBLE_STATUS_OK;
 }
 
 // We don't need to do anything on shutdown
-void mumble_shutdown() { }
+void mumble_shutdown()
+{
+}
 
 
 // Mumble boilerplate
-mumble_version_t mumble_getVersion() {
+mumble_version_t mumble_getVersion()
+{
 	mumble_version_t version;
 	version.major = 2;
 	version.minor = 0;
@@ -227,8 +256,9 @@ mumble_version_t mumble_getVersion() {
 	return version;
 }
 
-struct MumbleStringWrapper mumble_getAuthor() {
-	static const char* author = "checkraisefold, bonezone2001";
+struct MumbleStringWrapper mumble_getAuthor()
+{
+	static auto author = "checkraisefold, bonezone2001";
 
 	struct MumbleStringWrapper wrapper;
 	wrapper.data = author;
@@ -238,8 +268,10 @@ struct MumbleStringWrapper mumble_getAuthor() {
 	return wrapper;
 }
 
-struct MumbleStringWrapper mumble_getDescription() {
-	static const char* description = "A plugin using a websocket server to support ROBLOX. Context and identity are supported.";
+struct MumbleStringWrapper mumble_getDescription()
+{
+	static auto description =
+		"A plugin using a websocket server to support ROBLOX. Context and identity are supported.";
 
 	struct MumbleStringWrapper wrapper;
 	wrapper.data = description;
@@ -249,8 +281,9 @@ struct MumbleStringWrapper mumble_getDescription() {
 	return wrapper;
 }
 
-struct MumbleStringWrapper mumble_getName() {
-	static const char* name = "ROBLOXLink";
+struct MumbleStringWrapper mumble_getName()
+{
+	static auto name = "ROBLOXLink";
 
 	struct MumbleStringWrapper wrapper;
 	wrapper.data = name;
@@ -261,19 +294,24 @@ struct MumbleStringWrapper mumble_getName() {
 }
 
 
-mumble_version_t mumble_getAPIVersion() {
+mumble_version_t mumble_getAPIVersion()
+{
 	// This constant will always hold the API version that fits the included header files
 	return MUMBLE_PLUGIN_API_VERSION;
 }
 
-void mumble_registerAPIFunctions(void* apiStruct) { 
+void mumble_registerAPIFunctions(void* apiStruct)
+{
 	// Provided mumble_getAPIVersion returns MUMBLE_PLUGIN_API_VERSION, this cast will make sure
 	// that the passed pointer will be cast to the proper type
-	mumbleAPI = MUMBLE_API_CAST(apiStruct); 
+	mumbleApi = MUMBLE_API_CAST(apiStruct);
 }
 
-void mumble_releaseResource(const void* pointer) { }
+void mumble_releaseResource(const void* pointer)
+{
+}
 
-uint32_t mumble_getFeatures() {
+uint32_t mumble_getFeatures()
+{
 	return MUMBLE_FEATURE_POSITIONAL;
 }
